cd0e04a22b670b8c7f696f60f87b49c8a2922334
François Hodierne <francois@hodierne.net>
2022-12-02 11:21:21 +0100

    day 01 part two

--- day-01.js
+++ day-01.js
@@ -17,14 +17,26 @@ const getMaxFromInput = (array) => {
   return Math.max(...totals);
 };
 
+const getTopThreeTotalFromInput = (array) => {
+  const totals = array.map((e) => e.reduce((a, b) => a + b, 0));
+  totals.sort((a, b) => (a < b ? 1 : -1));
+  return totals.slice(0, 3).reduce((a, b) => a + b, 0);
+};
+
 const test = () => {
   const testInput = prepareInput(testIputFilename);
+
   assert.equal(getMaxFromInput(testInput), 24000);
+
+  assert.equal(getTopThreeTotalFromInput(testInput), 45000);
 };
 
 const run = () => {
   const input = prepareInput(inputFilename);
+
   console.log(getMaxFromInput(input));
+
+  console.log(getTopThreeTotalFromInput(input));
 };
 
 test();
